53cbde4bbc4b42c0ebaad48c1e62ad6d83f5069c
MChaparroCaballero <mchaparrocaballero@gmail.com>
2025-03-05 11:57:03 +0100

    cambiamos el gradle, y forzamos la orientacion a vertical siempre

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.examenmultimediamariachaparro"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.examenmultimediamariachaparro"
